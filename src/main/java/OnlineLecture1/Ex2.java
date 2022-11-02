8ce1baad61c65ba077bc73666fb54348a2c7c9a7
Anna-Karin <annakarinhamberg@outlook.com>
2022-11-02 16:27:41 +0100

    Initial commit

---
+++ src/main/java/OnlineLecture1/Ex2.java
@@ -0,0 +1,9 @@
+package OnlineLecture1;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        System.out.println("SHOPPING LIST \n \t-Milk \n \t\t>Semi-skinned \n \t\t>Medium \n \t -Eggs \n \t\t>Free-range \n\t -Bread");
+    }
+}
+
---
+++ src/main/java/OnlineLecture1/Ex3.java
@@ -0,0 +1,11 @@
+package OnlineLecture1;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println("\n\n The");
+        System.out.println("\t Cake");
+        System.out.println("Is");
+        System.out.println("\t\t A");
+        System.out.println("\t Lie.");
+    }
+}
